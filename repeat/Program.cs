e1cc63ab61ae1e5088a5651e76072dc37f1ad446
MG <maryamvh@code.edu.az>
2022-12-27 13:12:48 +0400

    done

---
+++ repeat/Program.cs
@@ -0,0 +1,99 @@
+﻿using System;
+
+namespace repeat
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            User[] users = new User[3];
+
+            users[0] = new User("Abbas", "Dajweoi1");
+            users[1] = new User("Hikmet", "A2opkoi1");
+            users[2] = new User("Ali", "23Fikswe");
+
+            string input;
+
+            do
+            {
+                Console.WriteLine("\n1.User əlavə et");
+                Console.WriteLine("2.Userlere bax");
+                Console.WriteLine("3.Userler üzrə axtarış et");
+
+                input = Console.ReadLine();
+
+                if (input == "1")
+                {
+                    InsertUser(ref users, CreateUser());
+
+                }
+
+                else if (input == "2")
+                {
+                    ShowUsersInfo(users);
+                }
+
+                else if (input == "3")
+                {
+                    Console.WriteLine("Axtaris deyerini daxil edin:");
+                    string find = Console.ReadLine();
+
+                    var filter = searchUser(ref users, find);
+                    ShowUsersInfo(filter);
+                } 
+
+            } while (input != "1" || input != "2" || input != "3");
+        }
+
+        static void ShowUsersInfo(User[] arr)
+        {
+            for (int i = 0; i < arr.Length; i++)
+            {
+                Console.WriteLine(arr[i].Showinfo());
+            }
+        }
+
+        static User[] searchUser(ref User[] arr, string find)
+        {
+            User[] newArr = new User[0];
+            for (int i = 0; i < arr.Length; i++)
+            {
+                if (arr[i].UserName.Contains(find, StringComparison.OrdinalIgnoreCase))
+                {
+                    Array.Resize(ref newArr, newArr.Length + 1);
+                    newArr[newArr.Length - 1] = arr[i];
+                }
+            }
+            return newArr;
+        }
+
+        static User[] InsertUser(ref User[] userArr, User user)
+        {
+            Array.Resize(ref userArr, userArr.Length + 1);
+            userArr[userArr.Length - 1] = user;
+
+            return userArr;
+        }
+
+        static User CreateUser()
+        {
+            User user = new User(string username, string password);
+
+            do
+            {
+                Console.Write("Username daxil edin");
+                user.UserName = (Console.ReadLine());
+
+            } while (user.UserName == null);
+
+            do
+            {
+                Console.Write("Pass daxil edin");
+                user.Password = Console.ReadLine();
+
+            } while (user.Password == null);
+
+            return user;
+        }
+    }
+}
---
+++ repeat/User.cs
@@ -0,0 +1,123 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace repeat
+{
+    internal class User
+    {
+        private string _username;
+        private string _password;
+
+
+        public User(string username, string password)
+        {
+            this.UserName = username;
+            this.Password = password;
+        }
+
+        public string UserName
+        { 
+            get => _username; 
+            
+            set 
+            {
+                if(IsUserName(value))
+                {
+                    
+                   _username = value;
+                }
+            }
+        }
+
+        public string Password
+        {
+            get => _password;
+            set
+            {
+                if (IsPass(value))
+                {
+                    _password = value;
+                }
+            }
+        }
+
+        public bool IsUserName(string name)
+        {
+            if(!string.IsNullOrWhiteSpace(name))
+            {
+                if (name.Length >= 6 && name.Length <= 25 )
+                {
+                    return true;
+                }
+                else
+                { return false; }
+            }
+            else { return false; }
+        }
+
+        public bool IsPassLength(string pass)
+        {
+            if (pass.Length >= 8 && pass.Length <= 25)
+            {
+                return true;
+            }
+            else
+            { return false; }
+
+        }
+
+        public bool HasLower(string pass)
+        {
+            for (int i = 0; i < pass.Length; i++)
+            {
+                if (char.IsLower(pass[i]))
+                {
+                    return true;
+                }
+            }
+            return false;
+        }
+
+        public bool HasUpper(string pass)
+        {
+            for (int i = 0; i < pass.Length; i++)
+            {
+                if (char.IsUpper(pass[i]))
+                {
+                    return true;
+                }
+            }
+            return false;
+        }
+
+        public bool HasDigit(string pass)
+        {
+            for (int i = 0; i < pass.Length; i++)
+            {
+                if (char.IsDigit(pass[i]))
+                {
+                    return true;
+                }
+            }
+            return false;
+        }
+
+        public bool IsPass(string pass)
+        {
+            if (!string.IsNullOrWhiteSpace(pass))
+            {
+                if (IsPassLength(pass) && HasUpper(pass) && HasLower(pass) && HasDigit(pass))
+                {
+                    return true;
+                }
+            }
+            return false;
+        }
+
+        public string Showinfo()
+        {
+            return $"\nUsername: {UserName} ";
+        }
+    }
+}
